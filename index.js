d7d48c5acaccc7a340dec6a817e4d715dd763a99
latedude2 <simasc10@gmail.com>
2021-06-06 10:54:43 +0200

    Add app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 5000
+const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
